68b01d9f2ebbd0eb1358cc4991ddd100b2ea126c
JVijayabharathi <46838405+JVijayabharathi@users.noreply.github.com>
2019-01-20 22:53:17 +0530

    Add files via upload

---
+++ ARR_MEDI.C
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,j,range,array[10],temp;
+clrscr();
+scanf("%d",&range);
+for(i=0;i<range;i++)
+  scanf("%d",&array[i]);
+for(i=0;i<range;i++)
+{
+  for(j=i+1;j<range;j++)
+  {
+   if(array[i]>array[j])
+    {
+      temp=array[i];
+      array[i]=array[j];
+      array[j]=temp;
+    }
+   }
+}
+if(range%2==0)
+ range/=2;
+else
+ {
+ range/=2;
+ range++;
+ }
+printf("%d",array[range-1]);
+getch();
+}
