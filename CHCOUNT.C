58c41d3511e75a4c7d78232b261bab6ba30cfc44
JVijayabharathi <46838405+JVijayabharathi@users.noreply.github.com>
2019-01-21 22:50:24 +0530

    Add files via upload

---
+++ CHCOUNT.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char string[50];
+int i,count=0;
+clrscr();
+scanf("%[^\n]s",string);
+for(i=0;string[i]!='\0';i++)
+{
+ if(string[i]!=' ')
+    count++;
+}
+printf("%d",count);
+getch();
+}
