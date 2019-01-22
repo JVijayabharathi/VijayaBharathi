a7e5ac555ed6ba5ff3b202b40b332dbb64cc102c
JVijayabharathi <46838405+JVijayabharathi@users.noreply.github.com>
2019-01-22 19:58:21 +0530

    Add files via upload

---
+++ FIBONACI.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int f1=1,f2=0,f3,i,n;
+clrscr();
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+ f3=f1+f2;
+ printf("%d ",f3);
+ f1=f2;
+ f2=f3;
+
+}
+getch();
+}
