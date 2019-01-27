3d913ef000a49a01d628b178b908fd8c0d606cfa
JVijayabharathi <46838405+JVijayabharathi@users.noreply.github.com>
2019-01-27 22:41:09 +0530

    Update BGINS545.C

--- BGINS545.C
+++ BGINS545.C
@@ -2,11 +2,12 @@
 #include<conio.h>
 void main()
 {
-long int number,count=0;
+long int number;
+ int c=0;
 clrscr();
 scanf("%ld",&number);
 for(;number>0;number/=10)
- count++;
-printf("%ld",count);
+ c++;
+printf("%d",c);
 getch();
 }
