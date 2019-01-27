57a8b2eeef5fcc36ed06c5d520a875ded9fce97e
JVijayabharathi <46838405+JVijayabharathi@users.noreply.github.com>
2019-01-27 18:50:51 +0530

    Add files via upload

---
+++ BGINS327.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char string[100];
+int i,flag=0;
+clrscr();
+scanf("%s",string);
+for(i=0;string[i]!='\0';i++)
+{
+if(string[i]>='0' && string[i]<='9')
+ flag++;
+}
+if(flag==0)
+ printf("No");
+else
+  printf("Yes");
+
+getch();
+}
