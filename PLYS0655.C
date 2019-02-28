66abc74ac2144af302044d76771dc815f1996426
JVijayabharathi <46838405+JVijayabharathi@users.noreply.github.com>
2019-02-28 12:18:49 +0530

    Create PLYS0655.C

---
+++ PLYS0655.C
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	char str[100],str2[100];
+	int i,n,m,c=0;
+	scanf("%s",&str);
+	scanf("%s",&str2);
+	n=strlen(str);
+	m=strlen(str2);
+	if(m!=n)
+	{
+		printf("no");
+	}
+	else
+	{
+	for(i=0;str[i]!='\0';i++)
+	{
+		if(str[i]<=92)
+		{
+			str[i]+=32;
+		}
+	}
+	for(i=0;str2[i]!='\0';i++)
+	{
+		if(str2[i]<=92)
+		{
+			str2[i]+=32;
+		}
+	}
+	for(i=0;str2[i]!='\0';i++)
+	{
+		if(str[i]==str2[i])
+		{
+			c+=1;
+		}
+	}
+	if(c==n)
+	{
+		printf("yes");
+	}
+}
+}
