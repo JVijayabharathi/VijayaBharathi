eada48d8f6d80a0c303b7d3b76edb9373ebd7804
JVijayabharathi <46838405+JVijayabharathi@users.noreply.github.com>
2019-03-14 23:11:56 +0530

    Create PLYS0770.C

---
+++ PLYS0770.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[100],b[100]="Answer";
+int i;
+scanf("%s",a);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i+1]=='\0')
+{
+printf("%c",a[i]);
+printf("%s",b);
+break;
+}
+else
+{
+printf("%c",a[i]);
+}
+}
+getch();
+}
