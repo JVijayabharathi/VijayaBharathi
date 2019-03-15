d46c135ccee70e4d46188adc6c3acc42e8959f14
JVijayabharathi <46838405+JVijayabharathi@users.noreply.github.com>
2019-03-15 06:03:56 +0530

    Create PLYS0876.C

---
+++ PLYS0876.C
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main()
+{
+int j,i,n,a[100],e=0,c=0,k;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+  scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+  if(a[i]%2==0)
+  {
+    e=e+1;
+    if(e>1)
+    {
+      for(j=0;j<n;j++)
+      {
+        if(a[j]%2!=0)
+        {
+          k=a[j];
+          break;
+        }
+      }
+    }
+  }
+  else
+  {
+    c=c+1;
+    if(c>1)
+      {
+      for(j=0;j<n;j++)
+      {
+        if(a[j]%2==0)
+        {
+        k=a[j];
+        break;
+        }
+      }
+    }
+  }
+}
+printf("%d",k);
+return 0;
+}
