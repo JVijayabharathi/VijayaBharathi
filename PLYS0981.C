a71d3b0cebe4efe1bee78953aece1f2a12528a3b
JVijayabharathi <46838405+JVijayabharathi@users.noreply.github.com>
2019-03-15 06:20:32 +0530

    Create PLYS0981.C

---
+++ PLYS0981.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int n,a[100],min,t,i,j;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	min=a[0]&a[i];
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			t=a[i]&a[j];
+			if(t>min)
+			{
+				min=t;
+			}
+		}
+	}
+	printf("%d",min);
+return 0;
+}
