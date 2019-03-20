71418f68ce37f08c075e07110731af6796e31a2f
JVijayabharathi <46838405+JVijayabharathi@users.noreply.github.com>
2019-03-20 22:19:17 +0530

    Create PLYS13126.C

---
+++ PLYS13126.C
@@ -0,0 +1,52 @@
+#include<stdio.h>
+int main()
+{
+ int n,a[50],i,j,count1=0,b[50],flag,k,count2=0,l=0,temp;
+ scanf("%d",&n);
+ scanf("%d",&k);
+ for(i=0;i<n;i++)
+ {
+  scanf("%d",&a[i]);
+ }
+ for(i=0;i<n;i++)
+ {
+  count1=0;
+  for(j=0;j<n;j++)
+  {
+   if(a[i]==a[j])
+   {
+    count1++;
+   }
+  }
+  if(count1<k)
+  {
+   b[l]=a[i];
+   count2++;
+   l++;
+  }
+ }
+ for(i=0;i<count2;i++)
+ {
+  flag=b[i];
+  l=i;
+  for(j=i+1;j<count2;j++)
+  {
+   if(b[j]<flag)
+   {
+    flag=b[j];
+    l=j;
+   }
+  }
+  if(k!=i)
+  {
+   temp=b[i];
+   b[i]=b[l];
+   b[l]=temp;
+  }
+ }
+ for(i=0;i<count2;i++)
+ {
+  printf("%d ",b[i]);
+ }
+ return 0;
+}
