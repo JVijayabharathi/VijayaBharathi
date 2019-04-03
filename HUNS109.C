5e262acc1cf1dc8a4f6e88f9ccebafcf3fac55a4
JVijayabharathi <46838405+JVijayabharathi@users.noreply.github.com>
2019-04-03 15:49:03 +0530

    Create HUNS109.C

---
+++ HUNS109.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+  int n,a[100],i,j,k;
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  for(i=0;i<n;i++)
+  {
+    for(j=i+1;j<n;j++)
+    {
+      if(a[i]+a[j]==0)
+      {
+        printf("%d %d",a[i],a[j]);
+      }
+    }
+  }
+
+  return 0;
+}
